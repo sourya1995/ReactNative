81b0708ec4133a4d9a3a9c03c61de82a4d66d1bd
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-19 18:30:25 +0530

    Adding initial components

---
+++ SampleProject/components/LittleLemonHeader.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native/types";
+
+export default function LittleLemonHeader() {
+    return (
+       
+        <View style={{flex: 0.2, backgroundColor: '#F4CE14'}}>
+            <Text style={{ padding: 40, fontSize: 30, color: 'black' }}>
+                Welcome to Little Lemon
+            </Text>
+        </View>
+    );
+}
