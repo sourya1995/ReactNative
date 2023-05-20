fbf4c95787b82d2bd01bc0458a0de1dbba67c5b5
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-20 10:01:58 +0530

    adding a small header change

--- SampleProject/components/LittleLemonHeader.js
+++ SampleProject/components/LittleLemonHeader.js
@@ -4,8 +4,9 @@ export default function LittleLemonHeader() {
     return (
        
         <View style={{flex: 0.2, backgroundColor: '#F4CE14'}}>
-            <Text style={{ padding: 40, fontSize: 30, color: 'black' }}>
-                Welcome to Little Lemon
+            <Text style={{ padding: 40, fontSize: 30, color: 'black' }} numberOfLines={3}>
+                Welcome
+                <Text style={{ fontWeight: 'bold' }}>Little Lemon</Text>
             </Text>
         </View>
     );
