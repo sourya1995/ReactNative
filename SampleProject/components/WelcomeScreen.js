0a365f458cf064456b2bcfb0a33c6be7b8e48186
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-20 18:37:15 +0530

    Adding input form

--- SampleProject/components/WelcomeScreen.js
+++ SampleProject/components/WelcomeScreen.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { View } from "react-native/types";
 
 export default function WelcomeScreen() {
+    const [firstName, onChangeFirstName] = useState('');
     return (
         <ScrollView indicatorStyle={"white"} style={styles.container}>
             <Text style={styles.headerText}>
@@ -10,6 +12,9 @@ export default function WelcomeScreen() {
                 Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a
                 lively but casual environment. We would love to hear your experience with us!
             </Text>
+            <TextInput style={styles.inputBox} value={firstName} onChangeText={onChangeFirstName} placeholder={'First Name'} />
+
+           
         </ScrollView>
     );
 }
@@ -30,5 +35,14 @@ const styles = StyleSheet.create({
         marginVertical: 8,
         color: '#EDEFEE',
         textAlign: 'center',
+    },
+    inputBox: {
+        height: 40,
+        margin: 12, 
+        padding: 10,
+        borderWidth: 1,
+        fontSize: 16,
+        backgroundColor: '#EDEFEE',
+        borderColor: '#EDEFEE',
     },
 });
