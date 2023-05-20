9c3346f25ac73b6b3a8e413148b39c0c5ee6eb60
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-20 16:38:23 +0530

    adding menu items FlatList

--- SampleProject/components/MenuItems.js
+++ SampleProject/components/MenuItems.js
@@ -1,18 +1,46 @@
-import { ScrollView, View } from "react-native/types";
+import { FlatList, ScrollView, View } from "react-native/types";
 
 const menuItemsToDisplay = [
-    'Hummus \n Moutabal \n Falafel \n Marinated Olives \n Kofta \n Eggplant Salad \n Lentil Burger \n Smoked Salmon \n Kofta Burger \n Turkish Kebab \n Fries \n Buttered Rice \n Bread Sticks \n Pita Pocket \n Lentil Soup \n Greek Salad \n Rice Pilaf \n Baklava \n Tartufo \n Tiramisu \n Panna Cotta',
-];
+    { name: 'Hummus', id: '1A' },
+    { name: 'Moutabal', id: '2B' },
+    { name: 'Falafel', id: '3C' },
+    { name: 'Marinated Olives', id: '4D' },
+    { name: 'Kofta', id: '5E' },
+    { name: 'Eggplant Salad', id: '6F' },
+    { name: 'Lentil Burger', id: '7G' },
+    { name: 'Smoked Salmon', id: '8H' },
+    { name: 'Kofta Burger', id: '9I' },
+    { name: 'Turkish Kebab', id: '10J' },
+    { name: 'Fries', id: '11K' },	
+    { name: 'Buttered Rice', id: '12L' },
+    { name: 'Bread Sticks', id: '13M' },
+    { name: 'Pita Pocket', id: '14N' },
+    { name: 'Lentil Soup', id: '15O' },
+    { name: 'Greek Salad', id: '16Q' },
+    { name: 'Rice Pilaf', id: '17R' },
+    { name: 'Baklava', id: '18S' },
+    { name: 'Tartufo', id: '19T' },
+    { name: 'Tartufo', id: '20U' },
+    { name: 'Tiramisu', id: '21V' },
+    { name: 'Panna Cotta', id: '22W' },
+  ];
+
+const Item = ({name}) => (
+    <View styles={menuStyles.innerContainer}>
+
+        <Text style={menuStyles.itemText}>
+            {name}
+        </Text>
+    </View>
+);
 
 const MenuItems = () => {
+
+    const renderItem = ({item}) => <Item name={item.name} />;
     return( 
-        <View style={{ flex: 0.75}}>
-            <ScrollView horizontal= {false} indicatorStyle={"white"} style={menuStyles.innerContainer}>
-                <Text style={menuStyles.headerText}>
-                    View Menu
-                </Text>
-                <Text style={menuStyles.itemText}>{ menuItemsToDisplay[0] }</Text>
-            </ScrollView>
+        <View style={menuStyles.container}>
+            <Text style={menuStyles.headerText}>View Menu</Text>
+            <FlatList data={menuItemsToDisplay} keyExtractor={(item) => item.id} renderItem={renderItem}></FlatList>
         </View>
     );
 };
@@ -32,6 +60,7 @@ const menuStyles = StyleSheet.create({
         color: 'white',
         fontSize: 40,
         flexWrap: 'wrap',
+        textAlign: 'center',
     },
     itemText: {
         color: '#F4CE14',
