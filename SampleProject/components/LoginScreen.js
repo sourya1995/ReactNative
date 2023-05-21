d863f14b6bc6e187de107f9445fecbc739a702a6
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-21 08:34:33 +0530

    Adding pressable login

--- SampleProject/components/LoginScreen.js
+++ SampleProject/components/LoginScreen.js
@@ -4,13 +4,23 @@ import { ScrollView } from "react-native/types";
 export default function LoginScreen() {
     const [email, onChangeEmail] = useState('');
     const [password, onChangePassword] = useState('');
+    const [loggedIn, onLoggedIn] = useState(false);
 
     return (
         <ScrollView style={styles.container}>
             <Text style={styles.headerText}>Welcome to Little Lemon</Text>
-            <Text style={styles.regularText}>Login to Continue</Text>
-            <TextInput style={styles.inputBox} value={email} onChangeText={onChangeEmail} placeholder={'email'} keyboardType={'email-address'} />
-            <TextInput value={password} onChangeText={onChangePassword} placeholder={'password'} secureTextEntry={true} />
+            {loggedIn && <Text style={styles.headerrText}>Login to Continue</Text>}
+            {!loggedIn && (
+                <>
+                    <Text style={styles.regularText}>Login to continue.</Text>
+                    <TextInput style={styles.inputBox} value={email} onChangeText={onChangeEmail} placeholder={'email'} keyboardType={'email-address'} />
+                    <TextInput value={password} onChangeText={onChangePassword} placeholder={'password'} secureTextEntry={true} />
+                    <Pressable onPress={() => onLoggedIn(!loggedIn)} style={styles.button}>
+                        <Text style={styles.buttonText}>Log in</Text>
+                    </Pressable>
+                </>
+            )}
+
         </ScrollView>
     );
 }
@@ -40,5 +50,20 @@ const styles = StyleSheet.create({
         borderColor: "#EDEFEE",
         backgroundColor: '#EDEFEE',
         fontSize: 16
-    }
+    },
+    button: {
+        fontSize: 22,
+        padding: 10,
+        marginVertical: 8,
+        margin: 100,
+        backgroundColor: '#EE9972',
+        borderColor: '#EE9972',
+        borderWidth: 2,
+        borderRadius: 50,
+    },
+    buttonText: {
+        color: 'black',
+        textAlign: 'center',
+        fontSize: 25,
+    },
 });
