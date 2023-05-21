2f04c1816523bce2dfc31a0b5f5b45f9c282d05e
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-21 09:48:06 +0530

    Adding image styling

--- SampleProject/components/WelcomeScreen.js
+++ SampleProject/components/WelcomeScreen.js
@@ -5,16 +5,19 @@ export default function WelcomeScreen() {
     const [firstName, onChangeFirstName] = useState('');
     return (
         <ScrollView indicatorStyle={"white"} style={styles.container}>
-            <Text style={styles.headerText}>
-                Welcome to Little Lemon
-            </Text>
+            <View style={styles.headerWrapper}>
+                <Image style={styles.image} source={require('./img/logo.png')} resizeMode="cover" accessible={true} accessibilityLabel={'Little Lemon Logo'} />
+                <Text style={styles.headerText}>
+                    Welcome to Little Lemon
+                </Text>
+            </View>
             <Text style={styles.regularText}>
                 Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a
                 lively but casual environment. We would love to hear your experience with us!
             </Text>
             <TextInput style={styles.inputBox} value={firstName} onChangeText={onChangeFirstName} placeholder={'First Name'} />
 
-           
+
         </ScrollView>
     );
 }
@@ -23,11 +26,21 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    headerWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        margin: 10,
+    },
     headerText: {
-        padding: 40,
+        paddingRight: 10,
+        paddingLeft: 20,
+        paddingTop: 30,
+        paddingBottom: 10,
         fontSize: 30,
         color: '#EDEFEE',
         textAlign: 'center',
+
+
     },
     regularText: {
         fontSize: 24,
@@ -36,9 +49,15 @@ const styles = StyleSheet.create({
         color: '#EDEFEE',
         textAlign: 'center',
     },
+    image: {
+
+        width: 100,
+        height: 100,
+        borderRadius: 20,
+    },
     inputBox: {
         height: 40,
-        margin: 12, 
+        margin: 12,
         padding: 10,
         borderWidth: 1,
         fontSize: 16,
